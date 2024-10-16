74ec4184a6be4d88f64f8e2682ee1d1916852477
Mega Octaviana <megap9039@gmail.com>
2024-10-16 10:58:44 +0700

    add index dan login

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); //merujuk ke file welcome.blade.ph di folder resource view
+});
+
+Route::get('/index', function () {
+    return view('index'); //merujuk ke file index.blade.ph di folder resource view
+});
+
+
+Route::get('/about', function () {
+    return view('Mega Octaviana'); //merujuk ke file about.blade.ph di folder resource view
+});
+
+Route::get('/login', function () {
+    return view('login'); //merujuk ke file welcome.blade.ph di folder resource view
 });
